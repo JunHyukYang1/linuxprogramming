a659bde75d50aff9d39b7b9c76d4dbf03f5c9e0f
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-04 21:48:48 +0900

    Add files via upload

---
+++ C++_Chapter_7-4/book1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Book {
+	string name;
+	int price;
+	int page;
+public:
+	Book(string name = {}, int price = 0, int page = 0) {
+		this->name = name, this->price = price, this->page = page;
+	}
+	void show();
+	Book& operator+=(int op1);
+	Book& operator-=(int op1);
+};
+
+void Book::show() {
+	cout << name << " " << price << "원 " << page << "페이지" << endl;
+}
+
+Book& Book::operator+=(int op1) {
+	this->price = this->price + op1;
+	return *this;
+}
+
+Book& Book::operator-=(int op1) {
+	this->price = this->price - op1;
+	return *this;
+}
+
+int main(void)
+{
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
+	a += 500;
+	b -= 500;
+	a.show();
+	b.show();
+}
---
+++ C++_Chapter_7-4/book2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Book {
+	string name;
+	int price;
+	int page;
+public:
+	Book(string name = {}, int price = 0, int page = 0) {
+		this->name = name, this->price = price, this->page = page;
+	}
+	void show();
+	friend Book& operator+=(Book& op1, int op2);
+	friend Book& operator-=(Book& op1, int op2);
+};
+
+void Book::show() {
+	cout << name << " " << price << "원 " << page << "페이지" << endl;
+}
+
+Book& operator+=(Book& op1, int op2) {
+	op1.price += op2;
+	return op1;
+}
+
+Book& operator-=(Book& op1, int op2) {
+	op1.price -= op2;
+	return op1;
+}
+
+int main(void)
+{
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
+	a += 500;
+	b -= 500;
+	a.show();
+	b.show();
+}
