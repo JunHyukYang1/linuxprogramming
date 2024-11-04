98d37fc1f4b4d354abee9ffc702a897307cdfb15
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-04 22:19:45 +0900

    Update Mul.hpp

--- C++_Chapter_7-4/실습과제3/Mul.hpp
+++ C++_Chapter_7-4/실습과제3/Mul.hpp
@@ -1,14 +1,14 @@
-#ifndef _MUL_HPP_
-#define _MUL_HPP_
+#ifndef _MUL_HPP_             // 헤더 파일의 중복 포함을 막기 위한 조건부 컴파일 지시자
+#define _MUL_HPP_             // _Mul_HPP_ 매크로 정의
 
-class Power {
-    int kick;
-    int punch;
+class Power {               // Power 클래스 선언
+    int kick;               // 멤버 변수 kick 선언
+    int punch;              // 멤버 변수 punch 선언
 public:
-    Power();
-    Power(int kick, int punch);
-    void show();
-    friend Power operator*(int op1, Power op2);
+    Power();                         // 멤버변수가 정해지지 않은 생성자 선언
+    Power(int kick, int punch);      // 멤버변수를 지정한 생성자 선언
+    void show();                         // 결과 출력을 위한 멤버 함수
+    friend Power operator*(int op1, Power op2);         // 정수, 객체 곱셈 연산을 위한 *연산자 재정의 프렌드 함수 선언
 };
 
 #endif
