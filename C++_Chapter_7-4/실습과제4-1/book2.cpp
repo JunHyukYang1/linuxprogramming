9fb8cdd4bbafc1eef7b93bfb2ca71f3af6989ef6
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-04 22:14:11 +0900

    Update book2.cpp

--- C++_Chapter_7-4/실습과제4-1/book2.cpp
+++ C++_Chapter_7-4/실습과제4-1/book2.cpp
@@ -15,7 +15,7 @@ public:
 };
 
 void Book::show() {
-	cout << name << " " << price << "¿ø " << page << "ÆäÀÌÁö" << endl;
+	cout << name << " " << price << "원 " << page << " 페이지" << endl;
 }
 
 Book& operator+=(Book& op1, int op2) {
@@ -30,7 +30,7 @@ Book& operator-=(Book& op1, int op2) {
 
 int main(void)
 {
-	Book a("Ã»Ãá", 20000, 300), b("¹Ì·¡", 30000, 500);
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
 	a += 500;
 	b -= 500;
 	a.show();
