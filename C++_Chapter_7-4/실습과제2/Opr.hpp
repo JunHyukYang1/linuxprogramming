c4cd6d11f735339cdbe2e5ed2d0f05e95991a274
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-04 16:37:56 +0900

    Update Opr.hpp

--- C++_Chapter_7-4/실습과제2/Opr.hpp
+++ C++_Chapter_7-4/실습과제2/Opr.hpp
@@ -1,14 +1,14 @@
-#ifndef _OPR_HPP_
-#define _OPR_HPP_
+#ifndef _OPR_HPP_            // 헤더 파일의 중복 포함을 막기 위한 조건부 컴파일 지시자
+#define _OPR_HPP_            // _OPR_HPP_ 매크로 정의
 
-class Complex {
-    int real;
-    int img;
+class Complex {                // Complex 클래스 선언
+    int real;                  // 멤버 변수 real 선언
+    int img;                   // 멤버 변수 img 선언
 public:
-    Complex(int real, int img);
-    void show();
-    friend Complex& operator++(Complex& op);
-    friend Complex operator--(Complex& op, int x);
+    Complex(int real, int img);                     // 생성자를 멤버 함수로 선언
+    void show();                                        // 결과 출력에 필요한 멤버 함수 선언
+    friend Complex& operator++(Complex& op);                 // 전위 증가 연산자를 재정의하기 위한 프렌드 함수 선언
+    friend Complex operator--(Complex& op, int x);           // 후위 감소 연산자를 재정의하기 위한 프렌드 함수 선언
 };
 
 #endif
