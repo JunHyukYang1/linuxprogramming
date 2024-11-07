e3da2ff2ce91da30ef3dbffdeabd9893d8284f1b
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-07 13:45:40 +0900

    Update Shape.hpp

--- C++_Chapter_8-1/실습과제1/Shape.hpp
+++ C++_Chapter_8-1/실습과제1/Shape.hpp
@@ -1,14 +1,14 @@
-#ifndef _SHAPE_HPP_
-#define _SHAPE_HPP_
+#ifndef _SHAPE_HPP_            // 헤더 파일 중복 포함 방지를 위한 조건부 컴파일 지시자
+#define _SHAPE_HPP_            // 매크로 정의
 
-class Shape {
-    int x, y;
+class Shape {                  // Shape 클래스 생성
+    int x, y;                  // priavate 멤버 변수 선언
 public:
-    void set(int x, int y);
-    void show_S();
+    void set(int x, int y);    // 변수에 값을 저장하는 멤버 함수 선언
+    void show_S();             // 결과 출력을 위한 멤버 함수 선언
 };
 
-class Circle : public Shape {
+class Circle : public Shape {         // 클래스 Shape를 상속하는 Circle 클래스 생성
     int r;
 public:
     void setCircle(int x, int y, int r);
