1eae6eab8e3d3d90d43a796d23eeb7cdfa5c8886
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-11 22:07:01 +0900

    Update TV.hpp

--- C++_Chapter_8-2/실습과제2/TV.hpp
+++ C++_Chapter_8-2/실습과제2/TV.hpp
@@ -1,30 +1,30 @@
-#ifndef _TV_HPP_
-#define _TV_HPP_
-#include <iostream>
+#ifndef _TV_HPP_              // 헤더 파일의 중복 포함을 방지하기 위한 조건부 컴파일 지시자
+#define _TV_HPP_              // 매크로 정의
+#include <iostream>           // 해당 코드 내에서는 string 클래스를 사용하기 위해 포함시킴
 using namespace std;
 
-class TV {
-    int size;
+class TV {           // TV 클래스 선언
+    int size;        // private 멤버 변수 size
 public:
-    TV();
-    void setTV(int size);
-    int getSize();
+    TV();                  // TV 클래스의 기본 생성자
+    void setTV(int size);        // 멤버 변수 size에 값을 저장하기 위한 멤버 함수 선언
+    int getSize();               // 멤버 변수 size에 저장된 값을 반환하기 위한 멤버 함수 선언
 };
 
-class WideTV : public TV {
-    bool videoIn;
+class WideTV : public TV {          // TV 클래스를 public으로 상속하는 클래스 WideTV 선언
+    bool videoIn;                   // private 멤버 변수 videoIn
 public:
-    WideTV();
-    void setWideTV(bool videoIn);
-    bool getVideoIn();
+    WideTV();                       // WideTV 클래스의 기본 생성자
+    void setWideTV(bool videoIn);      // 멤버 변수 videoIn에 값을 저장하기 위한 멤버 함수 선언
+    bool getVideoIn();                 // 멤버 변수 videoIn에 저장된 값을 반환하기 위한 멤버 함수 선언
 };
 
-class SmartTV : public WideTV {
-    string ipAddr;
+class SmartTV : public WideTV {       // WideTV 클래스를 public으로 상속하는 클래스 SmartTV 선언
+    string ipAddr;                    // private 멤버 변수 ipAddr
 public:
-    SmartTV();
-    void setSmartTV(int size, bool videoIn, string ipAddr);
-    string getIpAddr();
+    SmartTV();                       // SmartTV 클래스의 기본 생성자
+    void setSmartTV(int size, bool videoIn, string ipAddr);         // 각 멤버 변수에 값을 저장하기 위한 멤버 함수 선언
+    string getIpAddr();                                             // 멤버 변수 ipAddr에 저장된 값을 반환하기 위한 멤버 함수 선언
 };
 
 #endif
