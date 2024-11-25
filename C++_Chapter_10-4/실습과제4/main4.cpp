4b1d1f4d12e5c044140409490bfa53af43012c2a
JunHyukYang1 <yangjunh102881@gmail.com>
2024-11-25 15:55:08 +0900

    Update main4.cpp

--- C++_Chapter_10-4/실습과제4/main4.cpp
+++ C++_Chapter_10-4/실습과제4/main4.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <iostream>                                  // 표준 입출력 라이브러리를 사용하기 위한 헤더 파일 포함
+#include <vector>                                    // vector 템플릿 클래스를 사용하기 위한 헤더 파일 포함
+#include <algorithm>                                 // 알고리즘 함수를 사용하기 위한 헤더 파일 포함
+using namespace std;                                 // 모든 이름의 표준 라이브러리를 사용하기 위한 구문
 
-class Student{
-    string name;
-    int score;
+class Student{                                       // Student 클래스 정의
+    string name;                                     // 이름을 저장할 멤버 변수 name
+    int score;                                       // 점수를 저장할 멤버 변수 score
 public:
     Student(string name, int score) { this->name = name, this->score = score; }
     bool operator< (Student& op2) {
