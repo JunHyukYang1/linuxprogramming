2ac04ea7bf8d3c9cd52f7e15137796089df49c8f
JunHyukYang1 <yangjunh102881@gmail.com>
2024-12-02 15:46:40 +0900

    Update main1.cpp

--- C++_Chapter_10-6/실습과제1/main1.cpp
+++ C++_Chapter_10-6/실습과제1/main1.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <iostream>                                           // C++에서 표준 입출력 라이브러리를 사용하기 위한 헤더 파일 포함
+#include <vector>                                             // vector 템플릿 클래스를 사용하기 위한 헤더 파일 포함
+using namespace std;                                          // 모든 이름의 표준 라이브러리를 사용하기 위한 구문
 
 int main()
 {
-    vector<vector<int>> v1 = {{2, 4}, {5, -5}};
-    vector<vector<int>> v2 = {{-2, 3}, {0, -5}};
-    vector<vector<int>> result;
+    vector<vector<int>> v1 = {{2, 4}, {5, -5}};               // vector 클래스를 이용해 만든 2차원 배열 객체 v1, 생성과 함께 초기화
+    vector<vector<int>> v2 = {{-2, 3}, {0, -5}};              // vector 클래스를 이용해 만든 2치원 배열 객체 v2, 생성과 함께 초기화
+    vector<vector<int>> result;                               // 연산 수행 후 결과를 저장하기 위한 2차원 배열 객체 result
 
     for (int i = 0; i < v1.size(); i++)
     {
